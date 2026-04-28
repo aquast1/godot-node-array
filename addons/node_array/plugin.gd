f118f0b4453643feba2379c7a661a411124b4678
Alex Quast <alexiquast@gmail.com>
2026-04-27 18:33:50 -0600

    Add NodeArray2D custom type and icon import
    Register a new NodeArray2D custom type in the plugin: preload node_array_2d.gd and its SVG icon and add remove_custom_type on exit. Also add a .import file for icons/node_array_2d.svg to import it as a CompressedTexture2D with the project's texture import settings.

--- addons/node_array/plugin.gd
+++ addons/node_array/plugin.gd
@@ -9,7 +9,14 @@ func _enter_tree() -> void:
 		preload("node_array_3d.gd"),
 		preload("icons/node_array_3d.svg")
 	)
+	add_custom_type(
+		"NodeArray2D",
+		"Node2D",
+		preload("node_array_2d.gd"),
+		preload("icons/node_array_2d.svg")
+	)
 
 
 func _exit_tree() -> void:
 	remove_custom_type("NodeArray3D")
+	remove_custom_type("NodeArray2D")
